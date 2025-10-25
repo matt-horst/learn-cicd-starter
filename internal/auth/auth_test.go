db71fc58e6b89978fcc172001ca559fd7988f50a
Matt Horst <mattahorst@gmail.com>
2025-10-25 13:54:19 -0600

    adds tests for auth/GetApiKey

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,53 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetAIPKey(t *testing.T) {
+	cases := [] struct {
+		name string
+		header http.Header
+		expectErr bool
+		expectedApiKey string
+	} {
+		{
+			name: "Valid Header",
+			header: http.Header{"Authorization": []string{"ApiKey secret"}},
+			expectErr: false,
+			expectedApiKey: "secret",
+		},
+		{
+			name: "Missing Authorization",
+			header: http.Header{},
+			expectErr: true,
+			expectedApiKey: "",
+		},
+		{
+			name: "Missing ApiKey key",
+			header: http.Header{"Authorization": []string{}},
+			expectErr: true,
+			expectedApiKey: "",
+		},
+		{
+			name: "Missing ApiKey value",
+			header: http.Header{"Authorization": []string{"ApiKey"}},
+			expectErr: true,
+			expectedApiKey: "",
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			apiKey, err := GetAPIKey(c.header)
+			if (err == nil) == c.expectErr {
+				t.Errorf("Expected err == %v, but got %v", c.expectErr, err)
+			}
+
+			if (apiKey != c.expectedApiKey) {
+				t.Errorf("Expected apiKey = %v, but got %v", c.expectedApiKey, apiKey)
+			}
+		})
+	}
+}
